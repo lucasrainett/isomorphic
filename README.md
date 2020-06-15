11254407c5e35cc974482697530f6b9c9b45b8af
Lucas <lucas@MacBook-Pro.localdomain>
2020-06-15 15:59:45 +1200

    Add Readme and favicon

---
+++ README.md
@@ -0,0 +1,4 @@
+#Isomorphic Applications
+###with no Frameworks/Libraries
+
+[![Live Stream on Youtube](https://img.youtube.com/vi/5EVIqQmWrxA/2.jpg)](https://www.youtube.com/watch?v=5EVIqQmWrxA)
--- src/server/index.ts
+++ src/server/index.ts
@@ -8,13 +8,13 @@ http.createServer((request, response) => {
 
     const {pathname} = url.parse(request.url!);
 
-
     response.write(`
     <!doctype html>
     <html lang="en">
     <head>
     
     <meta charset="UTF-8">
+    <link rel="shortcut icon" type="image/x-icon" href="data:image/x-icon;,">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
